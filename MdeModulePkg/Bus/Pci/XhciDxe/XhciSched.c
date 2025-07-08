b5c9f8ef09cc436748c3f39d7b6709a50cdd3dea
ReleaseRaptor <ReleaseRaptor@cixtech.com>
2025-07-08 15:57:02 +0800

    edk2: usb: fix the bug of mass storage lost
    Change-Id: If7311bcaa5310c6fac2f881da58966cf43dca558

--- MdeModulePkg/Bus/Pci/XhciDxe/XhciSched.c
+++ MdeModulePkg/Bus/Pci/XhciDxe/XhciSched.c
@@ -1106,6 +1106,8 @@ XhcCheckUrbResult (
   UINTN                 Index;
   UINT8                 TRBType;
   EFI_STATUS            Status;
+  UINT32             Offset;
+  UINT32             State;
   URB                   *AsyncUrb;
   URB                   *CheckedUrb;
   UINT64                XhcDequeue;
@@ -1194,6 +1196,9 @@ XhcCheckUrbResult (
         CheckedUrb->Result  |= EFI_USB_ERR_TIMEOUT;
         CheckedUrb->Finished = TRUE;
         DEBUG ((DEBUG_ERROR, "XhcCheckUrbResult: TRANSACTION_ERROR! Completecode = %x\n", EvtTrb->Completecode));
+        Offset = XHC_PORTSC_OFFSET + 0x10;
+        State  = XhcReadOpReg (Xhc, Offset);
+        XhcWriteOpReg (Xhc, Offset, State);
         goto EXIT;
 
       case TRB_COMPLETION_STOPPED:
